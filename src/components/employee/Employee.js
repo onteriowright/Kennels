249c715e8f0d34af0e206a75de55be992c6cacfc
onteriowright <onteriowright@yahoo.com>
2020-01-22 19:40:20 -0600

    first commit

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <section className="employee">
+    <h3 className="employee__name">Emma Beaton</h3>
+    <div className="employee__location">Nashille Kennels North</div>
+  </section>
+);
---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <section className="location">
+    <h3 className="location__name">Nashille Kennels North</h3>
+    <div className="location__address">200 Main Street</div>
+  </section>
+);
